81bd418d2f8ad1c096aeab8f4182e3c4c6f7e8bb
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:19:18 -0500

    Update Left_Shift.vhd

--- Left_Shift.vhd
+++ Left_Shift.vhd
@@ -1,73 +1,46 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/23/2023 03:42:42 PM
--- Design Name: 
--- Module Name: Left_Shift - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Left_Shift is
-  Port (x : in std_logic_vector(7 downto 0);
-        left_shift_flag : in std_logic_vector(2 downto 0);
-        y : out std_logic_vector(7 downto 0));
+  Port (x : in std_logic_vector(7 downto 0); -- Takes in a 8 bit x and a 3 bit y for the # of shifts
+        left_shift_flag : in std_logic_vector(2 downto 0); 
+        y : out std_logic_vector(7 downto 0)); -- Outputs a 8 bit
 end Left_Shift;
 
 architecture Behavioral of Left_Shift is
-component multiplexer
+component multiplexer -- Calls a multiplxer for high and low
 Port (x, y, sel : in std_logic;
         output : out std_logic);
 end component;
 
-signal temp : std_logic_vector(7 downto 0);
-signal left_shift_flagt : std_logic_vector(2 downto 0);
+signal temp : std_logic_vector(7 downto 0); -- Temp signals
+signal flagTemp : std_logic_vector(2 downto 0); -- Temp Flag
 
 begin
 
--- if the left shift flag = 000 or 111 --> do nothing
--- if the left shift flag = 100 --> do not preform shift
-U1: multiplexer port map('0', x(0), left_shift_flag(2), temp(0));
-U2: multiplexer port map('0', x(1), left_shift_flag(2), temp(1));
-U3: multiplexer port map('0', x(2), left_shift_flag(2), temp(2));
-U4: multiplexer port map('0', x(3), left_shift_flag(2), temp(3));
-U5: multiplexer port map('0', x(4), left_shift_flag(2), temp(4));
-U6: multiplexer port map('0', x(5), left_shift_flag(2), temp(5));
-U7: multiplexer port map('0', x(6), left_shift_flag(2), temp(6));
-U8: multiplexer port map('0', x(7), left_shift_flag(2), temp(7));
+-- Calls the Multi based on the FlagTemp
+-- the MultiPlexer truth table is 
+  -- (0,0,0) And (0,0,1) and (0,1,1) and (1,0,0) -> 0
+  -- ( 0,1,0 ) and (1,0,1 ) and ( 1,1,0) and (1,1,1) ->1
+U1: multiplexer port map('0', x(0), flagTemp(2), temp(0));
+U2: multiplexer port map('0', x(1), flagTemp(2), temp(1));
+U3: multiplexer port map('0', x(2), flagTemp(2), temp(2));
+U4: multiplexer port map('0', x(3), flagTemp(2), temp(3));
+U5: multiplexer port map('0', x(4), flagTemp(2), temp(4));
+U6: multiplexer port map('0', x(5), flagTemp(2), temp(5));
+U7: multiplexer port map('0', x(6), flagTemp(2), temp(6));
+U8: multiplexer port map('0', x(7), flagTemp(2), temp(7));
 
--- if the left shift flag = 010 --> preform left shift
-U9: multiplexer port map( temp(0), '0', left_shift_flag(1), y(0));
-U10: multiplexer port map(temp(1), x(0), left_shift_flag(1), y(1));
-U11: multiplexer port map(temp(2), x(1), left_shift_flag(1), y(2));
-U12: multiplexer port map(temp(3), x(2), left_shift_flag(1), y(3));
-U13: multiplexer port map(temp(4), x(3), left_shift_flag(1), y(4));
-U14: multiplexer port map(temp(5), x(4), left_shift_flag(1), y(5));
-U15: multiplexer port map(temp(6), x(5), left_shift_flag(1), y(6));
-U16: multiplexer port map(temp(7), x(6), left_shift_flag(1), y(7));
-
+--Calls the Multi based on the FlagTemp
+  -- Follows the truth table from above
+U9: multiplexer port map( temp(0), '0', flagTemp(1), y(0));
+U10: multiplexer port map(temp(1), x(0), flagTemp(1), y(1));
+U11: multiplexer port map(temp(2), x(1), flagTemp(1), y(2));
+U12: multiplexer port map(temp(3), x(2), flagTemp(1), y(3));
+U13: multiplexer port map(temp(4), x(3), flagTemp(1), y(4));
+U14: multiplexer port map(temp(5), x(4), flagTemp(1), y(5));
+U15: multiplexer port map(temp(6), x(5), flagTemp(1), y(6));
+U16: multiplexer port map(temp(7), x(6), flagTemp(1), y(7));
 
 end Behavioral;
