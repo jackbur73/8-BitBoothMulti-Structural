47734d6bd3464f41d8a1d0ce1d609c237443079a
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:21:15 -0500

    Update and rename OR_GATE.vhd to OR2.vhd

--- OR2.vhd
+++ OR2.vhd
@@ -4,11 +4,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity OR2 is
-  Port (A, B : in std_logic; 
-  C : out std_logic);
+  Port (A, B : in std_logic; -- Takes in 2 in
+  C : out std_logic); -- has 1 out
 end OR2;
 
 architecture structural of OR2 is
 begin
-    C <= a or b ;
+    C <= a or b ; -- out is a or b
 end structural;
