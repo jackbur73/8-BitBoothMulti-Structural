d8c1b299b279c31b6351146dd3791ac20690a97c
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:32:08 -0500

    Update and rename XOR_GATE.vhd to XOR2.vhd

--- XOR2.vhd
+++ XOR2.vhd
@@ -3,11 +3,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity XOR2 is
-  Port (A, B : in std_logic; 
+  Port (A, B : in std_logic;  -- has 2 inputs
   C : out std_logic); -- Basic Xor Gate
-end XOR2;
+end XOR2; -- 1 output
 
 architecture structural of XOR2 is
 begin
-  C <= a xor b;
+  C <= a xor b; -- output is a xor b
 end structural;
