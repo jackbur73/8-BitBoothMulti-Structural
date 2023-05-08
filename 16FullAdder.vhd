28d678e47cbf29965aa4564ba3f14c14c30b254e
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:06:37 -0500

    Update and rename 16bit_csa.vhd to 16FullAdder.vhd

--- 16FullAdder.vhd
+++ 16FullAdder.vhd
@@ -1,21 +1,21 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity sixteenFullAdder is
+entity sixteenFullAdder is -- Takes in 2 16 bit vectors
   Port (x : in std_logic_vector(15 downto 0);
         y : in std_logic_vector(15 downto 0);
-        Cin : in std_logic;
-        output : out std_logic_vector(15 downto 0);
-        Cout : out std_logic);
+        Cin : in std_logic; -- Cin
+        output : out std_logic_vector(15 downto 0); -- Output
+        Cout : out std_logic); -- Carry Out
 end sixteenFullAdder;
 
-architecture Behavioral of sixteenFullAdder is
+architecture Behavioral of sixteenFullAdder is -- It calls the full adder
 component FullAdder is
   Port (A, B, Cin : in std_logic;
         sum, Cout : out std_logic);
 end component;
 
-signal C0 : std_logic_vector(15 downto 0);
+signal C0 : std_logic_vector(15 downto 0); -- Temp Signal for the Carry out
 begin
 
 U1:  FullAdder port map(x(0), y(0), '0', output(0), C0(0)); 
@@ -23,8 +23,8 @@ U2:  FullAdder port map(x(1), y(1), C0(0), output(1), C0(1));
 U3:  FullAdder port map(x(2), y(2), C0(1), output(2), C0(2));
 U4:  FullAdder port map(x(3), y(3), C0(2), output(3), C0(3));
 U5: FullAdder port map(x(4), y(4), C0(3), output(4), C0(4));
-U6:  FullAdder port map(x(5), y(5), C0(4), output(5), C0(5));
-U7:  FullAdder port map(x(6), y(6), C0(5), output(6), C0(6));
+U6:  FullAdder port map(x(5), y(5), C0(4), output(5), C0(5)); -- the port mapping This gets called 16 times 
+U7:  FullAdder port map(x(6), y(6), C0(5), output(6), C0(6)); -- It is saved to the output vector
 U8:  FullAdder port map(x(7), y(7), C0(6), output(7), C0(7));
 U9:  FullAdder port map(x(8), y(8), C0(7), output(8), C0(8));
 U10: FullAdder port map(x(9), y(9), C0(8), output(9), C0(9));
