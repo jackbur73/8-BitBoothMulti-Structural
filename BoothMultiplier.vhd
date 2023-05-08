64fb7d9975a5a85a856fcdad1e60d625612109dc
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:11:18 -0500

    Update and rename Booth_Multiplier.vhd to BoothMultiplier.vhd

--- BoothMultiplier.vhd
+++ BoothMultiplier.vhd
@@ -3,21 +3,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Booth_Multiplier is
-  Port (RegA : in std_logic_vector(7 downto 0);
-        RegB : in std_logic_vector(7 downto 0);
-        Clk: in std_logic;
-        LoadFlag : in std_logic;
-        EndFlag :inout std_logic;
-        Regz : out std_logic_vector(15 downto 0));
+  Port (RegA : in std_logic_vector(7 downto 0); -- Reg A
+        RegB : in std_logic_vector(7 downto 0); -- Reg B
+        Clk: in std_logic; -- Clock for process
+        LoadFlag : in std_logic; -- L flag
+        EndFlag :inout std_logic; -- E Flag
+        Regz : out std_logic_vector(15 downto 0)); -- output
 end Booth_Multiplier;
 
-architecture Behavioral of Booth_Multiplier is
+architecture Behavioral of Booth_Multiplier is -- Calls the encoder
 component Encoder
 Port ( y : in std_logic_vector (2 downto 0);
            x : out std_logic_vector (2 downto 0));
 end component;
 
-component PP1
+component PP1 -- Calls the PP1 - PP4
 Port (inP : in std_logic_vector(7 downto 0);
         encoder_value : in std_logic_vector(2 downto 0);
         outP : out std_logic_vector(15 downto 0));
@@ -41,7 +41,7 @@ Port (inP : in std_logic_vector(7 downto 0);
         outP : out std_logic_vector(15 downto 0));
 end component;
 
-component sixteenFullAdder
+component sixteenFullAdder -- Calls 16FullAdder to add together the 4 PPs to A
 Port (x : in std_logic_vector(15 downto 0);
         y : in std_logic_vector(15 downto 0);
         Cin : in std_logic;
@@ -76,19 +76,19 @@ decode3 <= TempB(5 downto 3);
 decode4 <= TempB(7 downto 5);
 end if;
 
-end process;
+end process; 
 
 U1: Encoder port map(decode1, ret1);
-U2: Encoder port map(decode2, ret2);
+U2: Encoder port map(decode2, ret2); -- encoders the PP
 U3: Encoder port map(decode3, ret3);
 U4: Encoder port map(decode4, ret4);
 
 U5: PP1 port map(TempA, ret1, partTemp1);
-U6:  PP2 port map(TempA, ret2, partTemp2);
+U6:  PP2 port map(TempA, ret2, partTemp2); -- Creates the PPs
 U7:  PP3 port map(TempA, ret3, partTemp3);
 U8:  PP4 port map(TempA, ret4, partTemp4);
 
-U9: sixteenFullAdder port map(partTemp1, partTemp2, '0', Ftemp1, Cout);
+U9: sixteenFullAdder port map(partTemp1, partTemp2, '0', Ftemp1, Cout); -- Adds together the 4 PPs
 U10: sixteenFullAdder port map(Ftemp1, partTemp3, '0', Ftemp2, Cout);
 U11: sixteenFullAdder port map(Ftemp2, partTemp4, '0', Final, Cout);
 
