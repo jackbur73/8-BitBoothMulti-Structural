0b738de3fcd37c6ed49db32b33c03c417e1b26ed
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 01:19:57 -0500

    Update NOT_GATE.vhd

--- NOT_GATE.vhd
+++ NOT_GATE.vhd
@@ -1,14 +1,13 @@
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity NOT2 is -- Basic not Gate
-  Port (A : in std_logic; 
+  Port (A : in std_logic; -- Takes in 1 in and has 1 out
   outP  : out std_logic);
 end NOT2;
 
 architecture Behavioral of NOT2 is
 begin
-    outP <= not(a);
+    outP <= not(a); -- Out is Not input
 end Behavioral;
